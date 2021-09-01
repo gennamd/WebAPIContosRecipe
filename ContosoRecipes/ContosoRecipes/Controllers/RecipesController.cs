19a3db4054248465616fbdc8c717c12334787035
gennamd <89196323+gennamd@users.noreply.github.com>
2021-09-01 20:57:41 +0200

    Add files via upload
    [Genna] new commit.

---
+++ ContosoRecipes/ContosoRecipes/Controllers/RecipesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoRecipes.Controllers
+{
+    [Route("api/recipes")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        [HttpGet] // /api/recipes
+        public ActionResult GetRecipes()
+        {
+            string[] recipes = { "Pizza", "Chips", "Chicken Curry", "Sushi", "Burger", "Soup", "Pasta" };
+
+            if (recipes.Any())
+                return NotFound();
+            return Ok(recipes);
+        }
+
+        [HttpPost]
+        public ActionResult CreateNewRecipes()
+        {
+            string[] recipes = { "Pizza", "Chips", "Chicken Curry", "Sushi", "Burger", "Soup", "Pasta" };
+
+            if (recipes.Any())
+                return NotFound();
+            return Ok(recipes);
+        }
+
+        [HttpDelete("{id}")] // /api/recipes/1234
+        public ActionResult DeleteRecipes(string id)
+        {
+            bool badThingsHappned = false;
+
+            if (badThingsHappned)
+                return BadRequest();
+            return NoContent();
+        }
+
+    }
+}
